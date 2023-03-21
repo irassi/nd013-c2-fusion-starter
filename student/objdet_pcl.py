02c7004cdc3c5f3973a98267707c6c6af5e5520a
irassi <123716699+irassi@users.noreply.github.com>
2023-03-21 21:12:37 +0200

    ignore results/ + fix ID_S1_EX1

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -14,6 +14,8 @@
 import cv2
 import numpy as np
 import torch
+import zlib
+
 
 # add project directory to python path to enable relative imports
 import os
@@ -79,10 +81,11 @@ def show_range_image(frame, lidar_name):
     ri_range = ri_range * 255 / (np.amax(ri_range) - np.amin(ri_range))
 
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
+    #NORMALIZE MORE ?!?!
     ri_intensity = np.amax(ri_intensity)/2 * ri_intensity * 255 / np.amax(ri_intensity) - np.amin(ri_intensity)
 
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
-    ri_stack = np.vstack(ri_range,ri_intensity)
+    ri_stack = np.vstack((ri_range,ri_intensity))
     img_range_intensity = ri_stack.astype(np.uint8)
 
     #######
